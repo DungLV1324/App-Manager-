67f3a7cd96f578c7ec3444894750645e562efdc9
NguyenVietHoangmom <hoangnv2508@gmail.com>
2023-03-12 22:39:14 +0700

    pull ripple

--- app/src/main/java/com/example/filemanager/model/Trash.java
+++ app/src/main/java/com/example/filemanager/model/Trash.java
@@ -1,2 +1,35 @@
-package com.example.filemanager.model;public class Trash {
+package com.example.filemanager.model;
+
+public class Trash {
+    private String nameImage;
+    private  String dateImage;
+
+    public Trash(String nameImage, String dateImage) {
+        this.nameImage = nameImage;
+        this.dateImage = dateImage;
+    }
+
+    public String getNameImage() {
+        return nameImage;
+    }
+
+    public void setNameImage(String nameImage) {
+        this.nameImage = nameImage;
+    }
+
+    public String getDateImage() {
+        return dateImage;
+    }
+
+    public void setDateImage(String dateImage) {
+        this.dateImage = dateImage;
+    }
+
+    @Override
+    public String toString() {
+        return "Trash{" +
+                "nameImage='" + nameImage + '\'' +
+                ", dateImage='" + dateImage + '\'' +
+                '}';
+    }
 }
